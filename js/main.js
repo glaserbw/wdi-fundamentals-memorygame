4537ebf5a9d60d40ca6035590da3aa1f4bb650f8
unknown <glaserbw@gmail.com>
2018-07-17 16:31:57 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,54 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "cardThree";
-var cardFour = "cardFour";
-console.log("User flipped " + cardOne); 
+// array for cards
+var cards = [
+
+{
+	rank: 'queen',
+	suit: 'hearts',
+	cardImage: "images/queen-of-hearts.png"
+},
+
+{
+	rank: 'queen',
+	suit: 'diamonds',
+	cardImage: "images/queen-of-diamonds.png"
+},
+
+{
+	rank: 'king',
+	suit: 'hearts',
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: 'king',
+	suit: 'diamonds',
+	cardImage: "images/king-of-diamonds.png"
+}
+
+];
+
+// array for cards in play
+
+var cardsInPlay = [];
+
+var checkForMatch = function() {
+	// do they match?
+
+	if(cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.")
+	}
+
+var flipCard = function() {
+    var cardId = this.getAttribute('data-id');
+    console.log("The user flipped a " + cards[cardId].rank);
+    cardsInPlay.push(cards[cardId].rank);
+    this.setAttribute('src', cards[cardId].cardImage);
+    
+    if (cardsInPlay.length === 2) {
+        checkForMatch();
+    }
+ };
+
+
 
